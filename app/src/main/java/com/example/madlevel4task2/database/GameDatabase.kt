f331033a8b0d24a783caf3b26ca9b0c70edec4cb
frozenpeppers <joris-ebbelaar@hotmail.nl>
2020-10-03 21:30:37 +0200

    implemented converter, comparator and logic for playing the game

--- app/src/main/java/com/example/madlevel4task2/database/GameDatabase.kt
+++ app/src/main/java/com/example/madlevel4task2/database/GameDatabase.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.madlevel4task2.converter.Converters
 import com.example.madlevel4task2.dao.GameDao
 import com.example.madlevel4task2.model.Game
 
 @Database(entities = [Game::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class GameDatabase : RoomDatabase() {
 
     abstract fun gameDao(): GameDao
